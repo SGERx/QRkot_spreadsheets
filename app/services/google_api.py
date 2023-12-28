9b4b0325761b14de398c3df007051bfe3e745063
OutEastCheshire <winampagent007@mail.ru>
2023-12-28 16:24:48 +0300

    minor changes

--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,25 +1,23 @@
-# app/services/google_api.py
-
 from datetime import datetime
 
 from aiogoogle import Aiogoogle
 import copy
-# В секретах лежит адрес вашего личного google-аккаунта
+
 from app.core.config import settings
 from app.services.my_json import spreadsheet_body
 
-# Константа с форматом строкового представления времени
+
 FORMAT = "%Y/%m/%d %H:%M:%S"
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-    # Создаём экземпляр класса Resourse
+
     service = await wrapper_services.discover('sheets', 'v4')
-    # Формируем тело запроса
+
     now_date_time = datetime.now()
     current_spreadsheet_body = copy.deepcopy(spreadsheet_body)
     current_spreadsheet_body['properties']['title'] = f'Отчет на {now_date_time}'
-    # Выполняем запрос
+
     response = await wrapper_services.as_service_account(
         service.spreadsheets.create(json=current_spreadsheet_body)
     )
@@ -50,13 +48,13 @@ async def spreadsheets_update_value(
 ) -> None:
     now_date_time = datetime.now().strftime(FORMAT)
     service = await wrapper_services.discover('sheets', 'v4')
-    # Здесь формируется тело таблицы
+
     table_values = [
         ['Отчет от', now_date_time],
         ['Топ проектов по скорости закрытия'],
         ['Название проекта', 'Время сбора', 'Описание']
     ]
-    # Здесь в таблицу добавляются строчки
+
     for res in projects:
         new_row = [
             res['name'],
